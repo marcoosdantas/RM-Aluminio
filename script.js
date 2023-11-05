4763fa8385639130064d441cdfdf0890843a40c6
Marcos Dantas <2226973@aluno.univesp.br>
2023-11-05 17:55:44 -0300

    Implementação de Typescript e React

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById(form);
+const nome = document.getElementById(nome);
+const email = document.getElementById(email);
+const telefone = document.getElementById(telefone);
+const mensagem = document.getElementById(mensagem);
+
